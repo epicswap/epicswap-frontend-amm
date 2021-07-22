d0654c7d8a72112bb5400e81cc8bc61ac7e101c0
이진우 <binarywoo@epiclions.net>
2021-07-22 15:54:00 +0900

    update

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x167affEF52469f80970cCf2bA6616F8f9dc239a7',
+  cakeAddress: '0xbA01b008d1530baB2b0DFeFd7a883028EA21d14E',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x38fff1066d09c87ba1321f74b9dff15e2061d934'
+  lpAddress: '0x4ac44c09c61c2e61470a84eb5cf169d5c8b15521'
 }
 
 export default priceContracts
