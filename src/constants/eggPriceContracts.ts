239278d216c88c17b4f0a6c5a5991c9976385497
brad kim <brad.kim@epiclions.net>
2021-09-17 20:31:44 +0900

    change lp add

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xCe6124Cdd53E2DD85f91EFb33cA476158Fa45978',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0xc2b0b26597095d5f6a9289b25807b76077a0d9d8'
+  lpAddress: '0x3bf79bf0c50db9adbbe8a0f6f3a198288174fc76'
 }
 
 export default priceContracts
