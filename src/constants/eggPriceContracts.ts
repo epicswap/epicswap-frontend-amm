757afb5d68fa736a5a0c7f9ff21377698903e49b
brad kim <brad.kim@epiclions.net>
2021-09-17 20:06:46 +0900

    change address

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x182Db9378Eb8FB4Eee006074bF4377c3D932620e',
+  cakeAddress: '0xCe6124Cdd53E2DD85f91EFb33cA476158Fa45978',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0xc2b0b26597095d5f6a9289b25807b76077a0d9d8'
 }
