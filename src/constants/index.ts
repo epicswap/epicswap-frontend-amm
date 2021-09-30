ee25b8f8cb158c0e58762f8245b6becfe752e16c
brad kim <brad.kim@epiclions.net>
2021-09-30 15:09:52 +0900

    change address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,9 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 // import { bsc, fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 import { injected, bsc } from '../connectors'
 // TODO
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'// mainnet
+// export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'// mainnet v1
+export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'// mainnet v2
+
 // export const ROUTER_ADDRESS = '0xD99D1c33F9fC3444f8101754aBC46c52416550D1'// testnet
 
 
--- src/constants/v1/index.ts
+++ src/constants/v1/index.ts
@@ -4,7 +4,8 @@ import V1_EXCHANGE_ABI from './v1_exchange.json'
 import V1_FACTORY_ABI from './v1_factory.json'
 
 const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0xC07d4604400139108BbdB3076636365A385879eF', // TODO
+  // [ChainId.MAINNET]: '0xC07d4604400139108BbdB3076636365A385879eF', // TODO v1
+  [ChainId.MAINNET]: '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73', // TODO v2
   [ChainId.BSCTESTNET]: '0x6725F303b657a9451d8BA641348b6761A6CC7a17'
 }
 
